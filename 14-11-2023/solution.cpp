d0248f3d02922be3f133eaf6902743d8ed6e574e
jeet kanodia <jeetkanodia@gmail.com>
2023-11-14 18:13:51 +0530

    14-11-2023

---
+++ 14-11-2023/solution.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1, string s2)
+    {
+        // Your code here
+        int n = s1.size();
+        if (s2.size() != n)
+            return false;
+        string ans = s1 + s1;
+
+        return (ans).find(s2) != -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution obj;
+        cout << obj.areRotations(s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
