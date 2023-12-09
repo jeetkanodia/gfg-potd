30c1b3010b00ede359479afacde631b7e08590d6
jeet kanodia <jeetkanodia@gmail.com>
2023-12-09 18:01:36 +0530

    09-12-2023

---
+++ 09-12-2023/solution.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int sumdigit(int x)
+    {
+        int sum = 0;
+        while (x)
+        {
+            sum += x % 10;
+            x = x / 10;
+        }
+        return sum;
+    }
+
+    bool isPrime(int n)
+    {
+        if (n == 1)
+            return false;
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int smithNum(int n)
+    {
+        int sum = sumdigit(n);
+        int sum2 = 0;
+        if (isPrime(n))
+            return 0;
+        for (int i = 2; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                int s = sumdigit(i);
+                while (n > 0 && n % i == 0)
+                {
+                    sum2 += s;
+                    n = n / i;
+                }
+            }
+        }
+        if (sum == sum2)
+            return 1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+
+        cin >> n;
+
+        Solution ob;
+        cout << ob.smithNum(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
