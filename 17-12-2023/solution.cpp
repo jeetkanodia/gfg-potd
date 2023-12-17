4094a6c2504159a795aa880d302be6e75b5f6277
jeet kanodia <jeetkanodia@gmail.com>
2023-12-17 14:41:08 +0530

    17-12-2023

---
+++ 17-12-2023/solution.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	      if(n==1){
+            return arr[0];
+        }
+        int first=arr[0];
+        int second=max(arr[0],arr[1]);
+        int temp;
+        for(int i=2;i<n;i++){
+           temp=max(second,first+arr[i]);
+            first=second;
+            second=temp;
+        }
+        return second;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
