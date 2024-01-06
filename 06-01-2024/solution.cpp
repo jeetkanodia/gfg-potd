0845a3d45853e0c44651d81835610669ce8acdc6
jeet kanodia <jeetkanodia@gmail.com>
2024-01-06 21:35:19 +0530

    06-01-2024

---
+++ 06-01-2024/solution.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int func(int n)
+    {
+        int ans = 0;
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                ans++;
+                n = n / i;
+            }
+        }
+        if (n != 1)
+            ans++;
+        return ans;
+    }
+    int sumOfPowers(int a, int b)
+    {
+        // Code here
+        int ans = 0;
+
+        for (int i = a; i <= b; i++)
+        {
+            ans = ans + func(i);
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        int ans = obj.sumOfPowers(a, b);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
