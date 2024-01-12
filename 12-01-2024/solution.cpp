d6bf87dc72f9c174531e9463cc65c422bc7a3d73
jeet kanodia <jeetkanodia@gmail.com>
2024-01-12 17:40:29 +0530

    12-01-2024

---
+++ 12-01-2024/solution.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k)
+    {
+        // add code here.
+
+        vector<int> v;
+        for (int i = 0; i < k; i++)
+        {
+            v.push_back(q.front());
+            q.pop();
+        }
+        reverse(v.begin(), v.end());
+        while (!q.empty())
+        {
+            v.push_back(q.front());
+            q.pop();
+        }
+        for (auto i : v)
+            q.push(i);
+
+        return q;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0)
+        {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        Solution ob;
+        queue<int> ans = ob.modifyQueue(q, k);
+        while (!ans.empty())
+        {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
