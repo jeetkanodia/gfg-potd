0691f3ee55fe7a275ed2e65bb8cf6706c2f694f5
jeet kanodia <jeetkanodia@gmail.com>
2024-04-13 10:08:45 +0530

    13-04-2024

---
+++ 13-04-2024/solution.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    long long reversedBits(long long x)
+    {
+        // code here
+        long long ans = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            // cout<<ans<<endl;
+            ans = ans | (x & 1);
+            ans = ans << 1;
+            x = x >> 1;
+        }
+        ans = ans >> 1;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long X;
+
+        cin >> X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
