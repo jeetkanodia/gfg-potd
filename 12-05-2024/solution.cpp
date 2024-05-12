0f817263726968b47cf59971f8b3900d06fb81d0
jeet kanodia <jeetkanodia@gmail.com>
2024-05-12 13:11:46 +0530

    12-05-2024

---
+++ 12-05-2024/solution.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int minSteps(int d)
+    {
+        // code
+        int temp = 0, ans = 0;
+        while (temp < d)
+        {
+            ans++;
+            temp += ans;
+        }
+        if (temp == d)
+            return ans;
+        int diff = temp - d;
+        if (diff % 2 == 0)
+            return ans;
+        return ans % 2 == 0 ? ans + 1 : ans + 2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
